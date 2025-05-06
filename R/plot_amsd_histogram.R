96f1de9e1a62a087d8a8a8e4679977d9c29f76ae
sfhart33 <sfhart33@gmail.com>
2025-05-06 14:26:00 -0700

    README, remove %>%

--- R/plot_amsd_histogram.R
+++ R/plot_amsd_histogram.R
@@ -7,8 +7,7 @@
 #' @return A ggplot histogram
 #' @export
 plot_amsd_histogram <- function(output){
-  plot1 <- data.frame(sims = output$sims) %>%
-    ggplot2::ggplot(aes(sims)) +
+  plot1 <- ggplot2::ggplot(data.frame(sims = output$sims), aes(sims)) +
     geom_histogram()+
     geom_vline(xintercept = output$cosine, linetype = "dashed")+
     xlab("Cosine distance")+
